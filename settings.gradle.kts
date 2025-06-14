9c0a93625b12040a3ae0bfa44978ad8f90e23c30
Zymus <moore.zyle@gmail.com>
2025-06-14 13:12:10 -0700

    Remove commented project references

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,8 +3,6 @@ rootProject.name = "cultoftheancestormoth"
 include(
     ":bytestring-serializer",
     ":cotam",
-//    ":ktor-record-api",
-//    ":rom",
     ":serialization",
     ":serialization-html",
     ":terraform:azure-devops-project",
